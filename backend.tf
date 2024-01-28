644378f7297f20f1d6b9aaa10475a818672dfec9
srinivasmunugoti <142879175+srinivasmunugoti@users.noreply.github.com>
2024-01-28 15:57:28 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-
+terraform {
   backend "s3" {
     bucket         = "my-terraform-python-test"
     key            = "terraform/github/terraform.tfstate"
     region         = "us-east-1"
   
   }
-
+}
