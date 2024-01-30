99dacb0c3721a46db5a8d38c9c89dfd928fc0538
srinivasmunugoti <142879175+srinivasmunugoti@users.noreply.github.com>
2024-01-30 13:37:59 +0530

    Update aws-ec2.tf

--- aws-ec2.tf
+++ aws-ec2.tf
@@ -49,7 +49,7 @@ module "ec2_instance" {
   
   name = "jenkinsinstance"
   ami="ami-0a3c3a20c09d6f377"
-  instance_type          = "t2.medium"
+  instance_type          = "t2.micro"
   key_name               = "allinone.pem"
   monitoring             = false
   vpc_security_group_ids = [aws_security_group.sg_my_security_group.id]
