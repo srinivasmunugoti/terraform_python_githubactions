c9bcb2f6e3fbc89c49a26bd8500ff15b86937a79
srinivasmunugoti <142879175+srinivasmunugoti@users.noreply.github.com>
2024-01-30 15:21:45 +0530

    Update aws-ec2.tf

--- aws-ec2.tf
+++ aws-ec2.tf
@@ -54,7 +54,8 @@ module "ec2_instance" {
   
   name = "jenkinsinstance"
   ami="ami-0a3c3a20c09d6f377"
-  instance_type          = "t2.micro"
+  instance_type = "t2.micro"
+  key_name = "newkey3"
   monitoring             = false
   vpc_security_group_ids = [aws_security_group.sg_my_security_group.id]
   subnet_id              = module.vpc.public_subnets[0]
